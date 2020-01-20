4749955871ef5bb6ff273cc89d93427b2fb6c250
Ajja99 <31586859+Ajja99@users.noreply.github.com>
2020-01-20 13:37:25 +0100

    Add LoginPage

---
+++ ChatDesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,90 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatDesktopUI.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+
+        public bool IsErrorVisible
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        private string _errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set { _errorMessage = value; }
+        }
+
+        private string _userName = "";
+
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                _userName = value;
+                NotifyOfPropertyChange(() => UserName);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        private string _password = "";
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => UserName);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public async Task LogIn()
+        {
+            UserName = "";
+            Password = "";
+        }
+
+        public bool CanLogIn
+        {
+            get
+            {
+                bool output = false;
+                if (_userName?.Length > 0)
+                {
+                    output = true;
+                }
+                return output;
+            }
+        }
+
+
+        public void Register()
+        {
+
+        }
+
+        public bool CanRegister
+        {
+            get
+            {
+                return true;
+            }
+        } 
+
+    }
+    
+}
---
+++ ChatDesktopUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatDesktopUI.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+
+        public ShellViewModel()
+        {
+            ActivateItem(IoC.Get<LoginViewModel>());
+        }
+
+    }
+}
